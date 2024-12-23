56e5907d79917f1be1c81f981abd24abe8678fbd
braydon_harris <braydonh@kengarff.com>
2024-12-23 15:17:19 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Get azure secrets from keyvault github action
+
+This action gets the secrets from azure keyvault and sets them as environment variables in the action.
+
+# Setup
+
+```
+npm ci
+```
+
+Build the script
+```
+npm run build
+```
+
+This puts the compiled script in the `dist` folder
+
+# Usage
+
+copy the contents of the `dist` folder into the folder `custom-gitub-actions/azure-env-vars` of the repository you want to use the action in.
+
+Then add the following to your github action workflow file
+
+```yaml
+    - name: Get App Env Vars
+      uses: ./custom-github-actions/azure-env-vars
+      id: dev-vars
+      with:
+		keyvault_name: ${{ env.KEYVAULT_NAME }}	  	
+        env: ${{ env.ENV_NAME }}
+        app_name: ${{ env.APP }}
+```
+
+
+
---
+++ main.js
@@ -0,0 +1,45 @@
+import { DefaultAzureCredential } from '@azure/identity';  
+import { SecretClient } from '@azure/keyvault-secrets';  
+import core from '@actions/core';
+
+async function main(){
+	const keyVaultName = core.getInput('keyvault_name');
+	const app = core.getInput('app_name');
+	const env = core.getInput('env');
+
+	// Authenticate to Azure
+	const credential = new DefaultAzureCredential(); 
+	
+	const url = `https://${keyVaultName}.vault.azure.net`;  
+	const client = new SecretClient(url, credential);
+	console.log("Connected to Azure Key Vault");
+	const secrets = [];
+	for await (const secretProperties of client.listPropertiesOfSecrets()){
+		// do something with properties
+		const { name, properties, value } = await client.getSecret(secretProperties.name);
+		secrets.push({name, value, tags: properties['tags']});
+	}
+	console.log("Fetched all the secrets from Azure Key Vault");
+	// filter out the screts based on the tags from the input
+	const names = [];
+	for (let i = 0; i < secrets.length; i++) {
+		const secret = secrets[i];
+		const tags = secret.tags;
+		if ((tags && tags['env'] && tags['app_name'] && tags['key']) && (tags['env'] === env && tags['app_name'] === app) ) {
+			// add the secret to the context
+			// @ts-ignore
+			core.setSecret(secret['value']);
+			core.exportVariable(tags['key'], secret['value']);
+        	core.setOutput(tags['key'], secret['value']);
+			names.push(secret['name']);
+		}
+	}
+	console.log("Exported all the secrets to the context");
+	core.setOutput('secrets', names);
+}
+
+main().catch((err) => {
+	core.debug("Get secret failed with error: " + err);
+    core.setFailed(!!err.message ? err.message : "Error occurred in fetching the secrets.");
+	process.exit(1);
+});
